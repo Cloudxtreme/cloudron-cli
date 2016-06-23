46bf06989c6857202c25828fc43ce67e5dc01fbe
Johannes Zellner <johannes@cloudron.io>
2016-06-23 16:07:37 +0200

    The arguments hack is not needed since it is all subcommands

--- bin/machine.js
+++ bin/machine.js
@@ -17,18 +17,4 @@ program
     .command('ssh', 'Get remote SSH connection')
     .command('backup', 'Manage Cloudron backups');
 
-if (!process.argv.slice(2).length) {
-    program.outputHelp();
-} else { // https://github.com/tj/commander.js/issues/338
-    // deal first with global flags!
-    program.parse(process.argv);
-
-    var knownCommand = program.commands.some(function (command) { return command._name === process.argv[2] || command._alias === process.argv[2]; });
-    if (!knownCommand) {
-        console.error('Unknown command: ' + process.argv[2]);
-        process.exit(1);
-    }
-    return;
-}
-
 program.parse(process.argv);
